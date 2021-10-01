4ec27fbf492a49df6af56bc192c191a0ec95f957
Jin Choi <choij@Jins-Mac-mini.local>
2021-10-02 06:37:01 +0900

    k-armed bandits

--- ReinforcementLearning/Chapter-2/random_numbers.cc
+++ ReinforcementLearning/Chapter-2/random_numbers.cc
@@ -5,19 +5,21 @@
 #include <TCanvas.h>
 #include <TH1D.h>
 
-int main() {
+int main()
+{
 	std::random_device rd;
 	std::mt19937 generator(rd());
 	std::uniform_int_distribution<> die(1, 6);
 	std::uniform_real_distribution<> uniform(0., 1.);
 	std::normal_distribution<double> gaus(0., 1.);
-	
-	TCanvas* c = new TCanvas("c", "");
-	TH1D* h_die = new TH1D("h_die", "", 6, 1., 7.);
-	TH1D* h_uni = new TH1D("h_uni", "", 100, 0., 1.);
-	TH1D* h_gaus = new TH1D("h_gaus", "", 100, -5, 5);
 
-	for (int i = 0; i < 100000; i++) {
+	TCanvas *c = new TCanvas("c", "");
+	TH1D *h_die = new TH1D("h_die", "", 6, 1., 7.);
+	TH1D *h_uni = new TH1D("h_uni", "", 100, 0., 1.);
+	TH1D *h_gaus = new TH1D("h_gaus", "", 100, -5, 5);
+
+	for (int i = 0; i < 100000; i++)
+	{
 		h_die->Fill(die(generator));
 		h_uni->Fill(uniform(generator));
 		h_gaus->Fill(gaus(generator));
@@ -32,8 +34,6 @@ int main() {
 
 	h_gaus->Draw();
 	c->SaveAs("random/gauss.png");
-
-	delete c, h_die, h_uni, h_gaus;
 
 	return 0;
 }
